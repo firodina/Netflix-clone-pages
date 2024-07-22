70841ec4a42ce2b93d15b7134abad5929575fc42
Rowda Hassan <rowdahassan49@gmail.com>
2024-07-22 18:28:45 +0300

    Update index.js

--- netflix-clone-page/src/index.js
+++ netflix-clone-page/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+<meta http-equiv="Content-Security-Policy" content="default-src 'self'; img-src 'self' https://example.com; script-src 'self' https://example.com;">
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
